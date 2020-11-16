dadad3a2db76295d0c0dce53f7dffa172757a37f
nabeelshaikh7 <72862916+nabeelshaikh7@users.noreply.github.com>
2020-11-16 15:56:37 +0530

    added the content

--- index.php
+++ index.php
@@ -1,1 +1,52 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Nabeel shaikh</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="https://cdn.nabeelshaikh.xyz/assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="https://cdn.nabeelshaikh.xyz/assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<section id="main">
+						<header>
+							<img src="https://cdn.nabeelshaikh.xyz/images/avatar.jpg" alt="Hi" width="135" height="145">
+							<h1>Shaikh Nabeel</h1>
+							<p>Unlimited Free Hosting's admin</p>
+						</header>
+						
+						<footer>
+							<ul class="icons">
+								
+								<a href="https://www.instagram.com/_nabeelshaikh_/"><img src="https://cdn.nabeelshaikh.xyz/images/insta.png" alt="Instagram" width="56" height="56">
+								<a href="https://github.com/nabeelshaikh7"><img src="https://cdn.nabeelshaikh.xyz/images/github.png" alt="github" width="56" height="56">
+								<a href="https://nabeelshaikh.xyz"><img src="https://cdn.nabeelshaikh.xyz/images/blog.png" alt="github" width="56" height="56">	
+							</ul>
+						</footer>
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="copyright">
+							<li>&copy; Nabeel shaikh</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script>
+				if ('addEventListener' in window) {
+					window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-preload\b/, ''); });
+					document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+				}
+			</script>
+
+	</body>
+</html>
 
